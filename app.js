856d9f36a69d1e7faddcbd4e77f30bdc6295e56c
Мирослав Здравковић <miroslav.zdravkovic@gmail.com>
2017-04-19 15:03:33 +0200

    Add jQuery npm

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-alert('Hello World!');
+let people = require('./people.js');
+let $ = require('jquery');
+
+$.each(people, function(key, value){
+    $('body').append('<h1>'+people[key].name+'</h1>');
+});
---
+++ people.js
@@ -0,0 +1,25 @@
+/*
+module.exports = `
+    <div className="container">
+        <div className="row">
+            <h1>Kukuriku!!!----</h1>
+        </div>
+    </div>
+`;
+*/
+
+/*
+function getHello(){
+    return 'Hello from function!';
+}
+
+module.exports = getHello();
+*/
+
+let people = [
+    {name: 'John Doe'},
+    {name: 'Steve Smith'},
+    {name: 'Carol Wiliams'}
+];
+
+module.exports = people;
